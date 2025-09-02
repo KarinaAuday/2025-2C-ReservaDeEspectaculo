8d9498fe8efa8215aad853afb796a1ec362fcdf6
SOFIA ANDREA OLGUIN <sofiaolguin@gmail.com>
2025-09-01 23:34:30 -0300

    mejoramiento clase pelicula

--- 2025-2C-ReservaEspectaculoORT/Models/Pelicula.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Pelicula.cs
@@ -1,11 +1,13 @@
-﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+﻿using System.Collections;
+namespace _2025_2C_ReservaEspectaculoORT.Models
 {
+  
     public class Pelicula
-    { public string Descripcion { get; set }
+    { public string Descripcion { get; set; }
         public string Titulo { get; set; }
         public DateTime FechaLanzaiento { get; set; }
-
         public int Id { get; set; }
         public string Foto { get; set; }
+        public List<Funcion> Funciones { get; set; } = [];
     }
 }
