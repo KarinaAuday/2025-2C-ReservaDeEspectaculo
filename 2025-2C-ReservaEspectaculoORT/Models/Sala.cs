c701c88dedd515032e84f50cdfdb1071a7ae5213
Martina <martubullon@gmail.com>
2025-09-01 23:10:21 -0300

    Se crearon las clases Sala y TipoSala y sus propiedades

---
+++ 2025-2C-ReservaEspectaculoORT/Models/Sala.cs
@@ -0,0 +1,13 @@
+﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+{
+    public class Sala
+    {
+        public int CapacidadButacas { get; set; }
+
+        public int Id { get; set; }
+
+        public int Numero { get; set; }
+
+        public TipoSala TipoSalaId { get; set; }
+    }
+}
---
+++ 2025-2C-ReservaEspectaculoORT/Models/TipoSala.cs
@@ -0,0 +1,11 @@
+﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+{
+    public class TipoSala
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
