669a5de6df9594bda3225f97999414e1dcd1e08c
zakiel-go <zakielgo@gmail.com>
2025-09-01 22:58:48 -0300

    Creados los modelos: Funcion, Reserva.

---
+++ 2025-2C-ReservaEspectaculoORT/Models/Funcion.cs
@@ -0,0 +1,13 @@
+﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+{
+    public class Funcion
+    {
+        public int ButacasDisponibles { get; set; }
+        public bool Confirmada { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Id { get; set; }
+        public int PeliculaId { get; set; }
+        public int SalaId { get; set; }
+    }
+}
---
+++ 2025-2C-ReservaEspectaculoORT/Models/Reserva.cs
@@ -0,0 +1,12 @@
+﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+{
+    public class Reserva
+    {
+        public bool Activa { get; set; }
+        public int CantidadButacas { get; set; }
+        public int ClienteId { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int FuncionId { get; set; }
+        public int Id { get; set; }
+    }
+}
