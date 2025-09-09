750932ce68e2ea681c508391601b4e0c27c5bc94
zakiel-go <zakielgo@gmail.com>
2025-09-08 22:35:49 -0300

    Relaciones navegacionales.

--- 2025-2C-ReservaEspectaculoORT/Models/Funcion.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Funcion.cs
@@ -2,11 +2,14 @@
 {
     public class Funcion
     {
+        public int Id { get; set; }
         public int ButacasDisponibles { get; set; }
         public bool Confirmada { get; set; }
         public string Descripcion { get; set; }
         public DateTime Fecha { get; set; }
-        public int Id { get; set; }
+        public Sala Sala { get; set; }
+        public Pelicula Pelicula { get; set; }  
+        public List<Reserva> Reservas { get; set; }
         public int PeliculaId { get; set; }
         public int SalaId { get; set; }
     }
--- 2025-2C-ReservaEspectaculoORT/Models/Reserva.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Reserva.cs
@@ -2,11 +2,13 @@
 {
     public class Reserva
     {
+        public int Id { get; set; }
         public bool Activa { get; set; }
         public int CantidadButacas { get; set; }
-        public int ClienteId { get; set; }
         public DateTime FechaAlta { get; set; }
+        public Cliente Cliente { get; set; }
+        public Funcion Funcion { get; set; }
+        public int ClienteId { get; set; }
         public int FuncionId { get; set; }
-        public int Id { get; set; }
     }
 }
