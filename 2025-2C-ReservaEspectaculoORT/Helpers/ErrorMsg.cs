2fed5a91d19865185361b7ad237af8334eb3b692
Héctor de la fuente <delafuentehector738@gmail.com>
2025-09-13 20:33:56 -0300

    Creacion de la clase ErrorMsg e implementacion de DataAnnotations en la clase Persona

---
+++ 2025-2C-ReservaEspectaculoORT/Helpers/ErrorMsg.cs
@@ -0,0 +1,10 @@
+﻿namespace _2025_2C_ReservaEspectaculoORT.Helpers
+{
+    public static class ErrorMsg
+    {
+        public const string Required = "El campo {0} es obligatorio";
+        public const string SoloLetras = "El campo {0} solo puede contener letras";
+        public const string OchoDigitos = "El campo {0} solo puede tener 8 digitos";
+        public const string StringLenght = "El campo {0} debe tener entre {2} y {1} caracteres";
+    }
+}
--- 2025-2C-ReservaEspectaculoORT/Models/Persona.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Persona.cs
@@ -1,14 +1,33 @@
-﻿namespace _2025_2C_ReservaEspectaculoORT.Models
+﻿using _2025_2C_ReservaEspectaculoORT.Helpers;
+using System.ComponentModel.DataAnnotations;
+namespace _2025_2C_ReservaEspectaculoORT.Models
 {
     public class Persona
     {
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Apellido { get; set; }
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [Display(Name = "Documento Nacional de Identidad")]
+        [RegularExpression(@"^\{8}$", ErrorMessage = ErrorMsg.OchoDigitos)]
         public string Dni { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        public string FechaAlta { get; set; }
+        public DateTime FechaAlta { get; set; }
+
+        [Required (ErrorMessage = ErrorMsg.Required)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Nombre { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         public string Telefono { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [StringLength(10, ErrorMessage = ErrorMsg.StringLenght)]
         public string UserName { get; set; }
     }
 }
