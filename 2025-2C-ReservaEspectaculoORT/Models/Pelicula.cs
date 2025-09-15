2181f01696370cfe1bdd483e1c73f65d6d18360d
SOFIA ANDREA OLGUIN <sofiaolguin@gmail.com>
2025-09-14 23:30:28 -0300

    navegacionales y requerimientos agregados

--- 2025-2C-ReservaEspectaculoORT/Models/Pelicula.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Pelicula.cs
@@ -1,13 +1,29 @@
-﻿using System.Collections;
+﻿using _2025_2C_ReservaEspectaculoORT.Helpers;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
 namespace _2025_2C_ReservaEspectaculoORT.Models
 {
   
     public class Pelicula
-    { public string Descripcion { get; set; }
+    {
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = ErrorMsg.SoloLetras)]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
         public DateTime FechaLanzaiento { get; set; }
         public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
         public string Foto { get; set; }
+       
         public List<Funcion> Funciones { get; set; } = [];
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        public Genero Genero { get; set; }
     }
 }
