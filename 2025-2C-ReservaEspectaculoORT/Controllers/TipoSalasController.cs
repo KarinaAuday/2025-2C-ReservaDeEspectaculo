9aa65082f503655a5e4b50d932426a986a8bb427
Martina <martubullon@gmail.com>
2025-10-13 20:19:39 -0300

    Se agrego el controlador de Tipo de sala

---
+++ 2025-2C-ReservaEspectaculoORT/Controllers/TipoSalasController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _2025_2C_ReservaEspectaculoORT.Data;
+using _2025_2C_ReservaEspectaculoORT.Models;
+
+namespace _2025_2C_ReservaEspectaculoORT.Controllers
+{
+    public class TipoSalasController : Controller
+    {
+        private readonly ReservaEspectaculoContext _context;
+
+        public TipoSalasController(ReservaEspectaculoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TipoSalas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TipoSala.ToListAsync());
+        }
+
+        // GET: TipoSalas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoSala = await _context.TipoSala
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipoSala == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoSala);
+        }
+
+        // GET: TipoSalas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoSalas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Precio")] TipoSala tipoSala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipoSala);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoSala);
+        }
+
+        // GET: TipoSalas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoSala = await _context.TipoSala.FindAsync(id);
+            if (tipoSala == null)
+            {
+                return NotFound();
+            }
+            return View(tipoSala);
+        }
+
+        // POST: TipoSalas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Precio")] TipoSala tipoSala)
+        {
+            if (id != tipoSala.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipoSala);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipoSalaExists(tipoSala.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoSala);
+        }
+
+        // GET: TipoSalas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoSala = await _context.TipoSala
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipoSala == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoSala);
+        }
+
+        // POST: TipoSalas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tipoSala = await _context.TipoSala.FindAsync(id);
+            if (tipoSala != null)
+            {
+                _context.TipoSala.Remove(tipoSala);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipoSalaExists(int id)
+        {
+            return _context.TipoSala.Any(e => e.Id == id);
+        }
+    }
+}
--- 2025-2C-ReservaEspectaculoORT/Data/ReservaEspectaculoContext.cs
+++ 2025-2C-ReservaEspectaculoORT/Data/ReservaEspectaculoContext.cs
@@ -12,6 +12,7 @@ namespace _2025_2C_ReservaEspectaculoORT.Data
         }
         public DbSet<_2025_2C_ReservaEspectaculoORT.Models.Persona> Persona { get; set; } = default!;
         public DbSet<_2025_2C_ReservaEspectaculoORT.Models.Sala> Sala { get; set; } = default!;
+        public DbSet<_2025_2C_ReservaEspectaculoORT.Models.TipoSala> TipoSala { get; set; } = default!;
        
 
     }
