c38576530f7cbf1679ae12827c684c53c998cc74
zakiel-go <zakiel9@hotmail.com>
2025-10-20 22:10:42 -0300

    Se quitaron restricciones para la propiedad Id de Funcion y Reserva.

--- 2025-2C-ReservaEspectaculoORT/Models/Funcion.cs
+++ 2025-2C-ReservaEspectaculoORT/Models/Funcion.cs
@@ -4,8 +4,6 @@ namespace _2025_2C_ReservaEspectaculoORT.Models
 {
     public class Funcion
     {
-        [Required(ErrorMessage = ErrorMsg.Required)]
-        [RegularExpression(@"^\d+$", ErrorMessage = ErrorMsg.SoloNumeros)]
         public int Id { get; set; }
 
         [Required(ErrorMessage = ErrorMsg.Required)]
